d2cb4860647ff1fdb55cce38508feb81ab22668d
Gazi-Md-Salauddin <mdsalauddin329132@gmail.com>
2026-05-22 17:58:48 +0600

    protect backend with jwt

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const verifyToken = async (req, res, next) => {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        //await client.connect();
         const db = client.db("pethaven");
         const petCollection = db.collection("pets");
         const requestCollection = db.collection("requests");
@@ -128,7 +128,7 @@ async function run() {
         });
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+       // await client.db("admin").command({ ping: 1 });
         console.log(
             "Pinged your deployment. You successfully connected to MongoDB"
         );
